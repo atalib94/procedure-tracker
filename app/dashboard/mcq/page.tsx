3a8e1f0b74a5915704f1b8c58d59b5471fb0521b
atalib94 <a.talib94@gmail.com>
2025-12-07 22:32:56 +0100

    Create page.tsx

---
+++ app/dashboard/mcq/page.tsx
@@ -0,0 +1,10 @@
+import EBIRMCQClient from '@/components/EBIRMCQClient'
+
+export const metadata = {
+  title: 'EBIR MCQ Practice | Procedure Tracker',
+  description: 'Practice EBIR exam questions by syllabus section',
+}
+
+export default function MCQPage() {
+  return <EBIRMCQClient />
+}
